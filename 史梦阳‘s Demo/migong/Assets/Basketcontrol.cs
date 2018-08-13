f1a597077102e1ae85c98e0fd912102a0cf54801
gamestarts <you@example.com>
2018-08-13 23:14:31 +0800

    first commit with Shi's demo

---
+++ 史梦阳‘s Demo/migong/Assets/Basketcontrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Basketcontrol : MonoBehaviour {
+    [Header("水平方向")]
+    public float moveX;
+    [Header("垂直方向")]
+    public float moveY;
+    [Header("推力")]
+    public float push;
+    Rigidbody2D rb;
+    [Header("分数文字UI")]
+    public Text countText;
+    [Header("过关文字UI")]
+    public Text winText;
+    int score = 0;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        winText.text = "";
+        setScoreText();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb.AddForce(push * movement);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(name+"触发了"+other.name);
+        if (other.CompareTag("apple"))
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+            setScoreText();
+        }
+    }
+    void setScoreText() {
+        countText.text = "目前分数" + score.ToString();
+        if (score >= 8)
+        {
+            winText.text = "你赢了";
+        }
+    }
+}
---
+++ 史梦阳‘s Demo/migong/Assets/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    [Header("玩家位移")]
+    public GameObject player;
+    [Header("相对位移")]
+    public Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
