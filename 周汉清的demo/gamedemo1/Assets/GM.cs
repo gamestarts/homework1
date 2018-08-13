803468c6ad2453384f9716d587656ed1f250f769
ZHqing-201611697 <2603863136@qq.com>
2018-08-14 01:25:23 +0800

    add 周汉清的demo

---
+++ 周汉清的demo/gamedemo1/Assets/GM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+    private bool isGameOver = false;
+    public Spawner spawner;
+    public Rotator rotator;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+    public void GameOver()
+    {
+        if (!isGameOver)
+        {
+            isGameOver = true;
+            spawner.enabled = false;
+            rotator.enabled = false;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ 周汉清的demo/gamedemo1/Assets/scripts/Pin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float speed = 20f;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.up * speed;
+
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "Rotator")
+        {
+            rb.velocity = Vector2.zero;
+            Score.scoreValue++;
+            transform.SetParent(col.transform);
+        }
+        else if (col.tag == "Pin")
+        {
+            GameObject.FindObjectOfType<GM>().GameOver();
+        }
+
+    }
+}
